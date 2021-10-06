22b1e2d357233c584e0c8c93840bbdd76a5a2560
leidyb <llbautistaleidy38@misena.edu.co>
2021-10-06 14:45:53 -0500

    Actividad Fracciones Js

--- javascript/fraccciones.js
+++ javascript/fraccciones.js
@@ -10,24 +10,24 @@ var frac={
         numeradorS2=this.n2 * this.d1;
         numeradorSF=numeradorS1 + numeradorS2;
         denominadorS=this.d1 * this.d2;
-        return 'el resultado de la suma es '+ numeradorSF + ' / ' + denominadorS;
+        return 'el resultado de la suma es '+ numeradorSF + ' /denominador es' + denominadorS;
     },
     resta:function(){
         numeradorR1=this.n1 * this.d2;
         numeradorR2=this.n2 * this.d1;
         numeradorRF=numeradorS1 - numeradorS2;
         denominadorR=this.d1 * this.d2;
-        return 'el resultado de la resta es '+ numeradorRF + ' / ' + denominadorR;
+        return 'el resultado de la resta es '+ numeradorRF + ' denominador es' + denominadorR;
     },
     multi:function(){
         numeradorM1=this.n1 * this.n2;
         numeradorM2=this.d1 * this.d2;
-        return 'el resultado de la multiplicacion es '+ numeradorM1 + ' / ' +numeradorM2;
+        return 'el resultado de la multiplicacion es '+ numeradorM1 + ' denominador es' +numeradorM2;
     },
     divi:function(){
         numeradorD1=this.n1 * this.d2;
         numeradorD2=this.n2 * this.d1;
-        return 'el resultado de la division es '+ numeradorD1 + ' / ' +numeradorD2;
+        return 'el resultado de la division es '+ numeradorD1 + ' denominador es' +numeradorD2;
     }
 }
 
