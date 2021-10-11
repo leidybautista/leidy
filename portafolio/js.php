83efe183426d8bc6a64aff5758445eb06539514e
leidyb <llbautistaleidy38@misena.edu.co>
2021-10-11 13:03:27 -0500

    Portafolio De Desarrollador terminado

---
+++ portafolio/js.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio</title>
+    <link rel="stylesheet" href="estilos/js.css">
+</head>
+
+<body>
+    <div class="Contenedor">
+        <div class="wrap">
+            <div class="img">
+                <img src="img/js.png" class="img" alt="">
+            </div>
+            <div class="titulo">
+                <br>
+                <label for="">Hola</label>
+                <label for="">algunos ejercicios en javascript</label>
+            </div>
+
+            
+            <br>
+            <br>
+            <div class="ContenedorIconos">
+                <a href="../portafolio/funciones/funciones-flecha.html" class="Link">
+                    <div class="Iconos">
+                        <p class="Centrar">Funciones flecha js </p>
+                    </div>
+                </a>
+                <a href="../portafolio/funciones/fraccionarios.html" class="Link">
+                    <div class="Iconos">
+                        <p class="Centrar">fraccionarios js</p>
+                    </div>
+                </a>
+                <a href="../portafolio/funciones/herencia-project.html" class="Link">
+                    <div class="Iconos">
+                        <p class="Centrar">herencia js</p>
+                    </div>
+                </a>
+                <a href="https://github.com/leidybautista/leidy/tree/main/javascript" class="Link">
+                    <div class="Iconos">
+                        <p class="Centrar">git hub js </p>
+                    </div>
+                </a>
+                <a href="https://github.com/leidybautista/leidy" class="Link">
+                    <div class="Iconos">
+                        <p class="Centrar">git hub Repositorio Completo </p>
+                    </div>
+                </a>
+                
+                    </div>
+                </a>
+            </div>
+            
+        </div>
+    </div>
+</body>
