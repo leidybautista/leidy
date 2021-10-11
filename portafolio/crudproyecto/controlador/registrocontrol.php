52734cf4ab95862997a94b7bcb1ee27329bbbb9b
leidyb <llbautistaleidy38@misena.edu.co>
2021-10-11 13:13:52 -0500

    Portafolio De Desarrollador terminado

--- portafolio/crudproyecto/controlador/registrocontrol.php
+++
@@ -1,8 +0,0 @@
-<?php 
- require('../dao/DaoComprasImpl.php');
-$dao=new DaoComprasImpl();
-$a=new Compras("tenis","85225","2","08-09-2021");
-//$dao->registrar($a);
- //echo '........'.var_dump($dao);
-require('../vista/index.php');
-?>
--- portafolio/crudproyecto/dao/daoCompras.php
+++
@@ -1,9 +0,0 @@
-<?php 
-interface DaoCompras{
-    public function registrar(Compras $a);
-    public function modificar(Compras $a);
-    public function eliminar(Compras $a);
-    //public function buscar($campo,$dato);
-    public function listar();
-}
-?>
